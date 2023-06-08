302f5cacd9d6c7adcd691c4dfbbb99bb53c6d5a5
abhi <manyypal@gmail.ccom>
2023-06-08 20:41:27 +0530

    signup validation

--- lib/signup.dart
+++ lib/signup.dart
@@ -4,9 +4,18 @@ import 'package:social_app/login.dart';
 class SignUpScreen extends StatelessWidget {
   SignUpScreen({super.key});
   final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
 
   submit() {
-    print(_usernameController.text);
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(_formKey.currentContext!).showSnackBar(SnackBar(
+        content: const Text('Form Submitted Successfully'),
+      ));
+    }
   }
 
   @override
@@ -16,79 +25,103 @@ class SignUpScreen extends StatelessWidget {
         title: const Text("Signup"),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                    labelText: "User Name",
-                    prefixIcon: const Icon(Icons.person)),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _usernameController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please etner a username';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      labelText: "User Name",
+                      prefixIcon: const Icon(Icons.person)),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                    labelText: "Mobile Number",
-                    prefixIcon: const Icon(Icons.call)),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _phoneNumberController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      labelText: "Mobile Number",
+                      prefixIcon: const Icon(Icons.call)),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                    labelText: "Email",
-                    prefixIcon: const Icon(Icons.email)),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _emailController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please etner a email';
+                    }
+                    RegExp emailFormt = RegExp(
+                        r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$');
+                    if (!emailFormt.hasMatch(value)) {
+                      return 'Please etner valid a email';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      labelText: "Email",
+                      prefixIcon: const Icon(Icons.email)),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                    labelText: "Password",
-                    prefixIcon: const Icon(Icons.lock)),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      labelText: "Password",
+                      prefixIcon: const Icon(Icons.lock)),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 50,
-                width: double.infinity,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        backgroundColor: Colors.red,
-                        textStyle: const TextStyle(fontSize: 25)),
-                    onPressed: () {
-                      submit();
-                    },
-                    child: const Text(
-                      "Sign up",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w500),
-                    )),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          backgroundColor: Colors.red,
+                          textStyle: const TextStyle(fontSize: 25)),
+                      onPressed: () {
+                        submit();
+                      },
+                      child: const Text(
+                        "Sign up",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w500),
+                      )),
+                ),
               ),
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                  );
-                },
-                child: const Text("Login")),
-          ],
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()),
+                    );
+                  },
+                  child: const Text("Login")),
+            ],
+          ),
         ),
       ),
     );
